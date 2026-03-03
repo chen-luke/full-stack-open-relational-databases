2b34c936d09cbe40e9f0657fa1d645e91554228f
Luke Chen <chen.luk@northeastern.edu>
2026-03-03 17:54:05 -0500

    feat: add bcrypt for password hashing in user creation (security improvement)

--- chapter-3-exercise/server/controllers/users.js
+++ chapter-3-exercise/server/controllers/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
 
 const { User, Note } = require('../models')
 
@@ -16,7 +17,17 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const user = await User.create(req.body)
+    const {username, name, password}= req.body
+
+    const saltRounds = 10
+
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = await User.create({
+        username,
+        name,
+        passwordHash
+    })
     res.json(user)
   } catch(error) {
     return res.status(400).json({ error })
