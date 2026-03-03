ef27783394b5a7f4b3a5456870b94a12c197a79d
Luke Chen <chen.luk@northeastern.edu>
2026-03-02 23:09:39 -0500

    feat(chapter-3): add user and login controllers, update models and database schema, and implement authentication using JSON Web Tokens (JWT) for secure communication between client and server. This change allows users to log in with a username and password, and receive a JWT token upon successful authentication. The token is used to authenticate subsequent requests to the API. Additionally, user and note models have been updated to include associations between them, allowing notes to be associated with specific users.

--- chapter-3/index.js
+++ chapter-3/index.js
@@ -6,10 +6,13 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const notesRouter = require('./controllers/notes')
+const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
-
 app.use('/api/notes', notesRouter)
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
