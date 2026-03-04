062f9ca4addea37b1b6419df9ef7de50b62d3efe
Luke Chen <chen.luk@northeastern.edu>
2026-03-04 09:26:34 -0500

    updated model index.js to have the correct logic and exports

--- chapter-3-exercise/server/models/index.js
+++ chapter-3-exercise/server/models/index.js
@@ -1,7 +1,15 @@
-const Blog = require('./blog.js')
+const Blog = require('./blog')
+const User = require('./user')
 
-Blog.sync()
+User.hasMany(Blog)
+Blog.belongsTo(User)
+
+// recreate the column during init if they were deleted
+// ORDER MATTERS HERE! BLOG HAS FOREIGN KEY REFERENCING USER
+// IF USER DOESN'T EXIST THEN ERROR!
+User.sync({alter: true})
+Blog.sync({alter: true})
 
 module.exports = {
-  Blog
+  Blog, User
 }
