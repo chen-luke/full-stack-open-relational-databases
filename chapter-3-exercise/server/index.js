aee62269ddffbfdc34b6487e34f32e99b99f8f8e
Luke Chen <chen.luk@northeastern.edu>
2026-03-05 17:15:38 -0500

    feat: add author controller to fetch top authors by number of articles and likes
    This commit introduces a new controller for handling author-related requests, specifically fetching the top authors based on the number of articles they have written and the total number of likes their articles received. The changes include creating a new router for the author controller, implementing the necessary functions to query the database using Sequelize, and adding the new route to the server's main file for handling requests at '/api/authors'. This will enable users to access information about popular authors on the platform.

--- chapter-3-exercise/server/index.js
+++ chapter-3-exercise/server/index.js
@@ -8,6 +8,7 @@ const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorRouter = require('./controllers/author')
 
 const { errorHandler } = require('./middleware/errorHandling')
 const { unknownEndpoint } = require('./middleware/unknownEndpoint')
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorRouter)
 
 app.use(unknownEndpoint)
 
