7e004c94688d7fb9f25360ea7dd4a92a8861b42a
Luke Chen <chen.luk@northeastern.edu>
2026-03-06 12:27:59 -0500

    feat(models/index.js): introduce initialize function to fix race conditions during setup of User and Blog models for CI/CD tests

--- chapter-3-exercise/server/models/index.js
+++ chapter-3-exercise/server/models/index.js
@@ -7,8 +7,16 @@ Blog.belongsTo(User)
 // recreate the column during init if they were deleted
 // ORDER MATTERS HERE! BLOG HAS FOREIGN KEY REFERENCING USER
 // IF USER DOESN'T EXIST THEN ERROR!
-await User.sync({ alter: true })
-await Blog.sync({ alter: true })
+
+const initialize = async () => {
+  await User.sync({ alter: true })
+  await Blog.sync({ alter: true })
+}
+
+// We need to use initialize to fix race conditions of setting up
+// User and Blog so that Blog doesn't complain it doesn't see User's foreign key
+// and crash the CI/CD github action test
+initialize()
 
 module.exports = {
   Blog, User
