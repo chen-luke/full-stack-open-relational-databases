c720feb06fd23c287a677a9b644cade048f1bb31
Luke Chen <chen.luk@northeastern.edu>
2026-03-06 12:20:34 -0500

    feat(models/index.js): change synchronization of User and Blog models to use await for better asynchronous handling

--- chapter-3-exercise/server/models/index.js
+++ chapter-3-exercise/server/models/index.js
@@ -7,8 +7,8 @@ Blog.belongsTo(User)
 // recreate the column during init if they were deleted
 // ORDER MATTERS HERE! BLOG HAS FOREIGN KEY REFERENCING USER
 // IF USER DOESN'T EXIST THEN ERROR!
-User.sync({alter: true})
-Blog.sync({alter: true})
+await User.sync({ alter: true })
+await Blog.sync({ alter: true })
 
 module.exports = {
   Blog, User
