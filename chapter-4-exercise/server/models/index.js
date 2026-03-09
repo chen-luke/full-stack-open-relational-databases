1c713287abbdd908779044b03f57b77b05ca2091
Luke Chen <chen.luk@northeastern.edu>
2026-03-09 10:17:06 -0400

    feat(chapter-4-exercise/server): add migrations for users and blogs tables with proper relationships
    * create migration files for users and blogs tables
    * define columns for each table including primary keys, foreign keys, and other necessary fields
    * set up relationships between the two tables using Sequelize's `hasMany` and `belongsTo` methods
    * update models to include new columns and relationships
    * turn off sync() since migrations are being used instead

--- chapter-4-exercise/server/models/index.js
+++ chapter-4-exercise/server/models/index.js
@@ -4,19 +4,22 @@ const User = require('./user')
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-// recreate the column during init if they were deleted
-// ORDER MATTERS HERE! BLOG HAS FOREIGN KEY REFERENCING USER
-// IF USER DOESN'T EXIST THEN ERROR!
+//*--------------------------------------------------------
+// Turrning sync() off since we are using migrations
 
-const initialize = async () => {
-  await User.sync({ alter: true })
-  await Blog.sync({ alter: true })
-}
+// // recreate the column during init if they were deleted
+// // ORDER MATTERS HERE! BLOG HAS FOREIGN KEY REFERENCING USER
+// // IF USER DOESN'T EXIST THEN ERROR!
 
-// We need to use initialize to fix race conditions of setting up
-// User and Blog so that Blog doesn't complain it doesn't see User's foreign key
-// and crash the CI/CD github action test
-initialize()
+// const initialize = async () => {
+//   await User.sync({ alter: true })
+//   await Blog.sync({ alter: true })
+// }
+
+// // We need to use initialize to fix race conditions of setting up
+// // User and Blog so that Blog doesn't complain it doesn't see User's foreign key
+// // and crash the CI/CD github action test
+// initialize()
 
 module.exports = {
   Blog, User
