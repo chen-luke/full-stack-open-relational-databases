dfcbf415732dd3b988868b4f04561f8c04d39441
Luke Chen <chen.luk@northeastern.edu>
2026-03-13 19:55:16 -0400

    feat(chapter-4-exercise/server/controllers/login.js): add token validation and session management feat(chapter-4-exercise/server/middleware/middleware.js): update token extraction to check for token validity using Session model

--- chapter-4-exercise/server/controllers/login.js
+++ chapter-4-exercise/server/controllers/login.js
@@ -3,15 +3,21 @@ const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const { SECRET } = require('../util/config')
 const User = require('../models/user')
+const Session = require('../models/session')
 
 router.post('/', async (request, response) => {
-  const {username, password} = request.body
+  const { username, password } = request.body
+
+  if (!username) {
+    return response.status(400).end()
+  }
 
   const user = await User.findOne({
     where: {
       username: username
     }
   })
+
 
   const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
@@ -21,12 +27,21 @@ router.post('/', async (request, response) => {
     })
   }
 
+
+  if (user.disabled) {
+    return response.status(401).json({ error: 'account disabled, please contact admin' })
+  }
+
   const userForToken = {
     username: user.username,
     id: user.id,
   }
 
   const token = jwt.sign(userForToken, SECRET)
+
+  // add token to session table to track
+
+  await Session.create({ userId: user.id, token: token })
 
   response
     .status(200)
