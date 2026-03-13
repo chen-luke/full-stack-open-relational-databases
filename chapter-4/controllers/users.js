6d56354281f83810df621bc0c9348d3bf3e90270
Luke Chen <72319988+chen-luke@users.noreply.github.com>
2026-03-13 14:56:46 -0400

    chapter 4 lecture.4 (#24)
    * feat(chapter-4/controllers/users.js): Introduce lazy loading for user's teams and add scopes to the User model for easier querying
    
    * Added lazy loading for user's teams in the `/users/:id` route, only searching for teams if a `teams=...?` query parameter is present.
    * Updated the `User` model to include defaultScope and scopes for easier querying:
     + Set the default scope to exclude disabled users (`disabled: false`)
     + Added admin, disabled, and name scopes for filtering users based on their admin status, disabled status, or name containing a specific value.
    
    * feat(chapter-4/controllers/users.js): add chaining scopes for finding users with the string 'jami' in their name and admin role
    feat(chapter-4/models/user.js): extend User model with methods for counting notes and filtering users with more than a specified number of notes

--- chapter-4/controllers/users.js
+++ chapter-4/controllers/users.js
@@ -4,6 +4,20 @@ const { User, Note, Team } = require('../models')
 const { tokenExtractor, isAdmin } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
+
+  // Used with scope from user model
+  // all admins
+  const adminUsers = await User.scope('admin').findAll()
+
+  // all inactive users
+  const disabledUsers = await User.scope('disabled').findAll()
+
+  // users with the string jami in their name
+  const jamiUsers = await User.scope({ method: ['name', '%jami%'] }).findAll()
+
+  // chaining scopes: admins with the string jami in their name
+  const jamiUsersChaining = await User.scope('admin', { method: ['name', '%jami%'] }).findAll()
+
   const users = await User.findAll({
     include: [
       {
@@ -32,46 +46,87 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.id, {
-      attributes: { exclude: [''] },
-      include: [
-        {
-          model: Note,
-          attributes: {
-            exclude: ['userId']
-          },
-        }, {
-          model: Note,
-          as: 'marked_notes',
-          attributes: { exclude: ['userId'] },
-          through: {
-            attributes: []
-          },
-          include: {
-            model: User,
-            attributes: ['name']
-          }
-        },
-        {
-          model: Team,
-          attributes: ['name', 'id'],
-          through: {
-            attributes: []
-          }
-        }
-      ]
-    })
-    if (user) {
-      res.json(user)
-    } else {
-      res.status(404).end()
-    }
-  } catch (error) {
-    next(error)
+// Lazy loading user's teams
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id, {
+    attributes: { exclude: [''] },
+    include: [{
+      model: Note,
+      attributes: { exclude: ['userId'] }
+    },
+    {
+      model: Note,
+      as: 'marked_notes',
+      attributes: { exclude: ['userId'] },
+      through: {
+        attributes: []
+      },
+      include: {
+        model: User,
+        attributes: ['name']
+      }
+    },
+    ]
+  })
+
+  if (!user) {
+    return res.status(404).end()
   }
+
+  // Lazy loading, only search for teams if we have team=...? query parameter.
+  let teams = undefined
+  if (req.query.teams) {
+    // user.getTeams() is automatically generated for us.
+    teams = await user.getTeams({
+      attributes: ['name'],
+      joinTableAttributes: []
+    })
+  }
+  res.json({ ...user.toJSON(), teams })
 })
+
+
+// None lazy loading user's teams
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     const user = await User.findByPk(req.params.id, {
+//       attributes: { exclude: [''] },
+//       include: [
+//         {
+//           model: Note,
+//           attributes: {
+//             exclude: ['userId']
+//           },
+//         }, {
+//           model: Note,
+//           as: 'marked_notes',
+//           attributes: { exclude: ['userId'] },
+//           through: {
+//             attributes: []
+//           },
+//           include: {
+//             model: User,
+//             attributes: ['name']
+//           }
+//         },
+//         {
+//           model: Team,
+//           attributes: ['name', 'id'],
+//           through: {
+//             attributes: []
+//           }
+//         }
+//       ]
+//     })
+//     if (user) {
+//       res.json(user)
+//     } else {
+//       res.status(404).end()
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
   const user = await User.findOne({
