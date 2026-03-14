c654ba481865408cb0cc5834eda7e6a4c15bb1cb
Luke Chen <chen.luk@northeastern.edu>
2026-03-14 12:03:16 -0400

    added session to reset

--- chapter-4-exercise/server/controllers/reset.js
+++ chapter-4-exercise/server/controllers/reset.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const { Blog, User } = require('../models')
+const { Blog, User, Session } = require('../models')
 
 
 router.post('/', async (req, res) => {
+    await Session.destroy({ truncate: true, cascade: true })
     await Blog.destroy({ truncate: true, cascade: true })
     await User.destroy({ truncate: true, cascade: true })
     res.status(200).json({ status: 'databases reseted' })
