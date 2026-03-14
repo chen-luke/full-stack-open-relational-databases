9464c07696ae190a7fd9342895d1b873f6e7db1e
Luke Chen <chen.luk@northeastern.edu>
2026-03-14 12:04:08 -0400

    added error console.log to developer terminal

--- chapter-4-exercise/server/middleware/errorHandling.js
+++ chapter-4-exercise/server/middleware/errorHandling.js
@@ -1,10 +1,14 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res) => {
 
     if (err.name === 'SequelizeValidationError') {
-        const errors = err.errors.map((error) => error.message) 
-        return res.status(400).json({ error: errors})
+        const errors = err.errors.map((error) => error.message)
+        return res.status(400).json({ error: errors })
     }
-    next(err)
+
+    console.error(err.stack) // only us developers can see this, we do not want to share this with the client
+
+    // client only sees "something went wrong"
+    res.status(500).json({ error: 'something went wrong' })
 }
 
 module.exports = { errorHandler }
